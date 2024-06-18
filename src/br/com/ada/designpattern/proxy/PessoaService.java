665b043b5f7b3d4ae6fd683dd576e4edf2d39fe9
Mauricio <mauriciom@protonmail.com>
2024-06-17 23:42:01 -0300

    Aula 6 - Proxy

---
+++ src/br/com/ada/designpattern/proxy/PessoaService.java
@@ -0,0 +1,21 @@
+package br.com.ada.designpattern.proxy;
+
+import br.com.ada.designpattern.builder.Pessoa;
+import br.com.ada.designpattern.proxy.solucao.PessoaRepositoryProxy;
+import br.com.ada.designpattern.proxy.solucao.ProxyPessoa;
+
+public class PessoaService {
+    private ProxyPessoa proxyPessoa;
+
+    public PessoaService(ProxyPessoa proxyPessoa) {
+        this.proxyPessoa = proxyPessoa;
+    }
+
+    public void save(Pessoa pessoa) {
+        proxyPessoa.save(pessoa);
+    }
+
+    public Pessoa findById(Long id) {
+        return proxyPessoa.findById(id);
+    }
+}
---
+++ src/br/com/ada/designpattern/proxy/solucao/ProxyPessoa.java
@@ -0,0 +1,8 @@
+package br.com.ada.designpattern.proxy.solucao;
+
+import br.com.ada.designpattern.builder.Pessoa;
+
+public interface ProxyPessoa {
+   void save(Pessoa pessoa);
+   Pessoa findById(Long id);
+}
